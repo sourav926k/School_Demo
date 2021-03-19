975b06741a20ba4c55362886d515f9dbb21c43da
Sourav Kumar <75413328+sourav926k@users.noreply.github.com>
2021-03-19 18:35:58 +0530

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-// Imported components
-import RouteHandlers from "./routes/RouteHandlers";
-
-ReactDOM.render(<RouteHandlers />, document.getElementById("root"));
